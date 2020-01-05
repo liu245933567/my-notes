555a43b8359a09bc8665f8fd6b5fa84f43be9098
刘永辉 <liuyonghui@jetair.com.cn>
2020-01-05 19:22:30 +0800

    chore: 添加文件

---
+++ app模版/蓝色手机触屏版医院网站模板/dongtai/js/ga.js
@@ -0,0 +1,78 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="format-detection" content="telephone=no">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<title>深圳博爱医院</title>
+<link rel="stylesheet" href="../../m.boai.com/css/style.css">
+<link rel="stylesheet" href="../../m.boai.com/css/common.css">
+<script src="../../m.boai.com/js/jquery-1.9.1.min.js"></script>
+<script src="../../m.boai.com/js/TouchSlide.1.1.js"></script>
+<script src="../../m.boai.com/js/iscroll.js"></script>
+</head>
+<body>
+<div id="warmp" class="warmp">
+<header class="header">
+	<section class="fix navbar">
+        <a class="ico-home" href="../../m.boai.com/default.htm"><span>返回首页</span></a>
+        <div id="title">深圳博爱医院</div>
+        <span class="ico-nav navHome">科室导航</span>
+    </section>
+    <h3 class="description">市医保定点医院　博爱<span>20</span>年品质保证</h3>
+</header>
+<article class="topic-con article notfound">	
+    <h1 class="hd">很抱歉，页面没有找到！</h1>
+	<div class="article-con">
+    	<p>可能是由于错误的网页地址或网站升级后页面地址变动所致，您可以：</p> 
+        <p class="tags"><a class="tel" onclick="msgOnline('call','foot');" href="tel://075525890396">电话咨询</a> <a href="javascript:history.go(-1);">返回上一页</a> <a href="../default.htm">回到首页</a>
+        </p>
+    </div>
+    
+    
+</article>
+
+</div>
+<!--//Wamrp-->
+<footer class="footer">
+	<menu class="footer-con">
+    	<ul class="fix">
+        	<li class="zx"><span onclick="msgOnline('chat','foot');">免费咨询</span></li>
+            <li class="tel"><a onclick="msgOnline('call','foot');" href="tel://075525890396">立即通话</a></li>
+            <li class="yy"><a href="../../wap.boai.com/yuyue_0_0.html">手机预约</a></li>
+            <li class="dh"><span class="navHome">科室导航</span></li>
+        </ul>
+	</menu>
+</footer>
+<nav id="nav" class="nav home">
+	<h3><a href="../../m.boai.com/default.htm">深圳博爱医院首页</a></h3>	
+    <div id="scrollerBox" class="scrollerBox">
+        <div class="scroller">
+            <ul>
+                <li><a href="../../meirongm.boai.com/"><span></span>激光美容科</a></li>
+                <li><a href="../../zhengxingm.boai.com/"><span></span>整形美容科</a></li>
+                <li><a href="../../kouqiangm.boai.com/"><span></span>口腔科</a></li>
+                <li><a href="../../yankem.boai.com/"><span></span>眼科</a></li>
+                <li><a href="../../fukem.boai.com/"><span></span>妇科</a></li>
+                <li><a href="../../chankem.boai.com/"><span></span>产科</a></li>
+                <li><a href="../../nankem.boai.com/"><span></span>男科</a></li>
+                <li><a href="../../pifum.boai.com/"><span></span>皮肤科</a></li>
+                <li><a href="../../waikem.boai.com/"><span></span>外科</a></li>
+                <li><a href="../../neikem.boai.com/"><span></span>内科</a></li>
+                <li><a href="../../erbihoum.boai.com/"><span></span>耳鼻喉科</a></li>
+                <li><a href="../"><span></span>乳腺科</a></li>
+                <li><a href="../../zhongyim.boai.com/"><span></span>中医科</a></li>
+                <li><a href="../../askm.boai.com/"><span></span>健康问答</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div id="nav-over"></div>
+<script src="../../m.boai.com/js/foot.js"></script>
+<script src="../../m.boai.com/js/swt.js"></script>
+</body>
+</html>
